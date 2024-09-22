c009bc1192878b619a26d98e16d5f4d94b0acac9
mustapha jamil <mustaphajay2@gmail.com>
2024-09-22 21:55:29 +0100

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\NotebookController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+// Route::get('notes', [NoteController::class, 'index']);
+
+Route::resource('notes', NoteController::class)->middleware('auth');
+Route::resource('notebooks', NotebookController::class)->middleware('auth');
